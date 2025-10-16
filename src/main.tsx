75df5a2e80d879de26f1a8dd0935319cddee77c0
김형수 <xkhs00@dankook.ac.kr>
2025-10-16 13:32:28 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,22 @@ import { createRoot } from "react-dom/client";
 import { AuthProvider } from "./hooks/useAuth";
 import App from "./App.tsx";
 import "./index.css";
+import { ThemeProvider } from "next-themes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { TooltipProvider } from "./components/ui/tooltip";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </TooltipProvider>
+      </QueryClientProvider>
+    </ThemeProvider>
   </StrictMode>
 );
