fd7db287803250c5832d5881995fc840deb4b2b8
김형수 <xkhs00@dankook.ac.kr>
2025-10-16 13:36:01 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useTheme } from "next-themes";
 import Auth from "./pages/Auth";
 import Schedule from "./pages/Schedule";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import Admin from "./pages/Admin";
 
-const App = () => (
-  <>
-    <Toaster />
-    <Sonner />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/schedule/:scheduleId" element={<Schedule />} />
-        <Route path="/admin" element={<Admin />} />
-        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </>
-);
+const App = () => {
+  const { theme } = useTheme();
+
+  return (
+    <>
+      <Toaster />
+      <Sonner theme={theme as "light" | "dark" | "system" | undefined} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/schedule/:scheduleId" element={<Schedule />} />
+          <Route path="/admin" element={<Admin />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
