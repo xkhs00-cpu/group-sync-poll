3ddf10b8a9e24a5e874fc9b9aa7c09b179aacebf
김형수 <xkhs00@dankook.ac.kr>
2025-10-16 13:35:37 +0900

    Update sonner.tsx

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,14 +1,10 @@
-import { useTheme } from "next-themes";
-import { Toaster as Sonner, toast } from "sonner";
+import { Toaster as SonnerPrimitive, toast } from "sonner";
 
-type ToasterProps = React.ComponentProps<typeof Sonner>;
+type ToasterProps = React.ComponentProps<typeof SonnerPrimitive>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
-
   return (
-    <Sonner
-      theme={theme as ToasterProps["theme"]}
+    <SonnerPrimitive
       className="toaster group"
       toastOptions={{
         classNames: {
